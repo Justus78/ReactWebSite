eede0293c30b206712542b033eed0ceefecb13a0
Justus78 <stars787878@aol.com>
2024-11-03 19:59:16 -0500

    added file structure to project

---
+++ my-website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'gatsby'
+
+const App = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
